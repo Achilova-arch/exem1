650d25df76a6ade3324e7fece66b93b93c21cf99
Achilova-arch <asalachilova96@gmail.com>
2025-09-30 08:56:29 +0500

    Add files via upload

---
+++ 10/main.py
@@ -0,0 +1,25 @@
+mevalar = ['kiwi','pineple','avacado','aplle','pear']
+print(mevalar)
+
+son=[]
+son.append(1)
+son.append(2)
+son.append(3)
+print(son)
+
+
+sonlar =[1,2,3,4,5,6,7,8,9,10]
+
+print(sonlar[-1])
+
+
+son1=[]
+son1.append(1)
+son1.append(2)
+son1.append(3)
+print(son1)
+
+sonla =[1,2,3,4,5,6,7,8,9,10]
+
+print(sonla[::-1])
+
